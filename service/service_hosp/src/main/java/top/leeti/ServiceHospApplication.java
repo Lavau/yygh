42ce9a75e7bc06fc2dd5bb9dcb094d79d75f1ebe
lavau <2723986904@qq.com>
2021-04-19 23:07:29 +0800

    该看P18了

--- service/service_hosp/src/main/java/top/leeti/ServiceHospApplication.java
+++ service/service_hosp/src/main/java/top/leeti/ServiceHospApplication.java
@@ -1,9 +1,11 @@
 package top.leeti;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("top.leeti.mapper")
 public class ServiceHospApplication {
     public static void main(String[] args) {
         SpringApplication.run(ServiceHospApplication.class, args);
--- service/service_hosp/src/main/java/top/leeti/controller/HospitalSetController.java
+++ service/service_hosp/src/main/java/top/leeti/controller/HospitalSetController.java
@@ -1,17 +1,37 @@
 package top.leeti.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import top.leeti.model.hosp.HospitalSet;
 import top.leeti.service.HospitalSetService;
 
+import javax.annotation.Resource;
+import java.util.List;
+
+@Api(tags = "医院设置管理")
 @RestController
 @RequestMapping("/admin/hosp/hospital")
 public class HospitalSetController {
 
-    @Autowired
+    @Resource
     private HospitalSetService hospitalSetService;
 
+    @ApiOperation(value = "获取所有医院设置")
+    @GetMapping("findAll")
+    public List<HospitalSet> findAllHospitalSet() {
+        List<HospitalSet> hospitals = hospitalSetService.list();
+        return hospitals;
+    }
 
+    @ApiOperation(value = "逻辑删除医院设置")
+    @GetMapping("{id}")
+    public boolean removeHospSet(@PathVariable Long id) {
+        boolean flag = hospitalSetService.removeById(id);
+        return flag;
+    }
 
 }
